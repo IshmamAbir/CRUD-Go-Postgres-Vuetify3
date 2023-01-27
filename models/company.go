ef9e91bf0d378456b5b1bf6be4d262b7b6ffb795
Ishmam Abir <i-chowdhury@kodensya.co.jp>
2023-01-27 09:29:14 +0900

    add-backend: set table name manually

--- models/company.go
+++ models/company.go
@@ -10,3 +10,8 @@ type Company struct {
 	Name     string    `json:"name" gorm:"type:varchar(100);not null"`
 	Location string    `json:"location" gorm:"type:varchar(200);not null"`
 }
+
+// this method set the table name when gorm creates the table
+func (Company) TableName() string {
+	return "company"
+}
